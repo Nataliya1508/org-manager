9e26cdf456b380a10b63d18125ca75f8f91bbc74
Nataly <97055353+Nataliya1508@users.noreply.github.com>
2024-02-11 00:45:47 +0200

    add pg db and type orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService): TypeOrmModuleOptions => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: Number(configService.get<number>('DATABASE_PORT')),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        synchronize: false,
+        ssl:
+          configService.get<string>('DATABASE_SSL_ENABLED') === 'false'
+            ? {
+                rejectUnauthorized:
+                  configService.get<string>('DATABASE_REJECT_UNAUTHORIZED') ===
+                  'false',
+              }
+            : undefined,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/database/migrations/**/*{.ts, .js}'],
+      }),
+      inject: [ConfigService],
+    }),
+    TypeOrmModule.forFeature([]),
+   ],
   controllers: [AppController],
   providers: [AppService],
 })
